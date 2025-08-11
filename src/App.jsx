a2a57035d466cfa4609ee77c96f5546576fa5f5e
Hossain-sakib <Hossain.sakib.x@gmail.com>
2025-08-11 16:18:48 +0600

    initialized the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import Watches from './components/Watches/Watches'
+
+function App() {
+
+
+  return (
+    <>
+      <Watches></Watches>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Watches/Watch/Watch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Watch = ({watch}) => {
+    console.log(watch);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Watch;
